c9a6a3d95105c0d8ac2020b1257068f49ba18ee3
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-09-09 19:44:22 +0430

    Pattern generator added

--- error_pattern_synthesizer.py
+++ error_pattern_synthesizer.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     symbols = int(symbols,10)
     symbolSize = raw_input("Enter Symbol size in bytes:  ")
     symbolSize = int(symbolSize,10)
-    print('/n at this moment we just support Hex representation')
+    print  'Up to this moment, we just support Hex representation (field is fixed to 8)'
     
 
 
     packetErrorRate = raw_input("Packets error rate(%):  ")
 
     packetErrorRate = int(packetErrorRate,10)
-    symbolErrorRate = raw_input("Symbols error rate: ")
+    symbolErrorRate = raw_input("Symbols error rate(%): ")
     symbolErrorRate = int(symbolErrorRate,10)
 
     fileName = "pktErr-"
@@ -27,18 +27,42 @@ if __name__ == "__main__":
     fileName +="-symErr-"
     fileName += str(symbolErrorRate)
     fileName += ".txt"
-    file= open("filename","w+")
-
+    file= open(fileName,"w+")
+    originalPacketList = []
     OriginalPacket = ''
     # two hex representation can be seen as a byte 
-    for i in range (0,symbolSize*2):
-        OriginalPacket += HEX_FIELD[random.randrange(0,16,1)]
+    for i in range (0,symbolSize):
+        tempA = HEX_FIELD[random.randrange(0,16,1)]
+        tempB = HEX_FIELD[random.randrange(0,16,1)]
+        tempSymbol = tempA + tempB
+        OriginalPacket += tempSymbol
+        originalPacketList.append(tempSymbol)
+
         
     print "The original Packet: ", OriginalPacket
 
     for i in range(0,symbols):
         # write error pattern into a file
-         file.write(OriginalPacket +"\n")
+        packetErrorChance = random.randrange(1,101,1)
+        if(packetErrorChance<packetErrorRate):
+            # we should change the symbol
+
+            errorPattern =''
+            for j in range (0 , symbolSize):
+                symbolErrorChance = random.randrange(1,101,1)
+                if(symbolErrorChance > symbolErrorRate ):
+                    error = ''
+                    firstRandHex =  HEX_FIELD[random.randrange(0,16,1)] 
+                    secondRandHex = HEX_FIELD[random.randrange(0,16,1)]
+                    error = firstRandHex + secondRandHex
+                    errorPattern +=error
+                else:
+                    errorPattern +=originalPacketList[j]
+            file.write(errorPattern+"\n")
+        else: 
+            file.write(OriginalPacket+"\n")
+
+         
 
     
 
