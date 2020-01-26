6b520545911ead52cd84fe00e7dcbf1db8046b7b
AmirHossein Nazari <hnirsnazari@yahoo.com>
2020-01-26 13:47:58 +0330

    Verification test done on LHBalancer

--- LHBalancer.py
+++ LHBalancer.py
@@ -41,9 +41,11 @@ def examination(NumberOfPackets,symbolErrorRate,eachPacketSize,generationSize):
     # expected Number of packet needed can be replace with Number of packets
 
     ''' 
-    limited[i]: Shows when the threshold is "i" How many of packets can be fixed in LimitedDecoder
-    hinted[i]: Shows when the threshold is "i" How many of packets can be fixed JUST in HintedDecoder
+    each packet size = k + r 
+    limited[i]: Shows when the threshold is "i" How many of "PACKETS" can be fixed in LimitedDecoder
+    hinted[i]: Shows when the threshold is "i" How many of "PACKETS" can be fixed JUST in HintedDecoder
     Full healthy: Number of packets with no errors
+    errorProbabilityAfterLimited[i]: records symbol errors and total symbols when the threshold is "i"
     '''
 
     # 
@@ -53,28 +55,26 @@ def examination(NumberOfPackets,symbolErrorRate,eachPacketSize,generationSize):
     totalNumberOfSymbols = 0
     iterationNumber = NumberOfPackets
 
-    # threshold can be equal to R at most 
-    maxThreshold = eachPacketSize - generationSize
+    # threshold can be equal to R at most , + 1 because it is starting from zero
+    maxThreshold = (eachPacketSize - generationSize) + 1
 
 
-    limited = [0] * generationSize
-    hinted = [0] * generationSize
+    limited = [0] * maxThreshold
+    hinted = [0] * maxThreshold
+    totallyBroken = [0] * maxThreshold
+
+
     errorProbabilityAfterLimited = []
 
 
 
-    for i in range (0,generationSize):
+    for i in range (0,maxThreshold):
         errorProbabilityAfterLimited.append(symbolErrorProbability())
 
     # count all symbols and then divide number of broken by all symbols
     errorProbabilityInGeneral=0
-
     FullHealthy = 0
-
     redundancy = eachPacketSize - generationSize
-
-    limitedFixingThreshold = generationSize 
-
     packetRepository = []
 
     for i in range (0,NumberOfPackets):
@@ -84,28 +84,40 @@ def examination(NumberOfPackets,symbolErrorRate,eachPacketSize,generationSize):
         #  here we should conut lots of thing
         numberOfError = tempPacket.getBrokenSymbolNumber()
 
+        if(numberOfError == 0 ):
+            FullHealthy +=1
 
         totalNumberOfSymbolErrors +=numberOfError
         totalNumberOfSymbols += eachPacketSize
 
-        for j in range(0,limitedFixingThreshold):
+        for j in range(0,maxThreshold):
             # here j is our threshold
             # errors with less than j errors are going to be fixed so we shouldn't count them here 
             if(numberOfError<=j):
                 limited[j] +=1
+                
             else:
-                errorProbabilityAfterLimited[j].increase(generationSize,numberOfError)
+                errorProbabilityAfterLimited[j].increase(eachPacketSize,numberOfError)
                 if(numberOfError<=redundancy):
                     hinted[j] +=1
-
+                else:
+                    totallyBroken[j] +=1
 
         packetRepository.append(tempPacket) 
 
     # we should count symbols error  after and before 
 
-    computedErrorRate = totalNumberOfSymbolErrors/totalNumberOfSymbols 
+    computedErrorRate = float(totalNumberOfSymbolErrors)/float(totalNumberOfSymbols) 
 
-    return limited , hinted , FullHealthy , errorProbabilityAfterLimited , computedErrorRate
+    #  convert Totally broken , limited , hinted, fullHealthy to percentage
+    for i in range ( 0, maxThreshold):
+        limited[i] = float(limited[i]) /float(NumberOfPackets)
+        hinted[i] = float(hinted[i]) / float(NumberOfPackets)
+        totallyBroken[i] = float(totallyBroken[i])/ float(NumberOfPackets)
+    
+    FullHealthy = float(FullHealthy) / float(NumberOfPackets)
+
+    return  limited , hinted , totallyBroken,  FullHealthy , errorProbabilityAfterLimited , computedErrorRate
 
 #  def GeneralExamination(NumberOfPackets,symbolErrorRate,eachPacketSize,generationSize ,numberOfExams):
 
@@ -137,7 +149,7 @@ if __name__ == "__main__":
     numberOfExams = int(raw_input("Examination Iteration: "))
 
     print "_______________________________________\n" 
-    for k in range (0,100):
-        print str(k) + "  Cycle finished ::::: "
-        examination(100000,k,31,26)
+    for symbolErrorRate in range (10,30,4):
+        print str(symbolErrorRate) + "  Cycle finished ::::: "
+        examination(6,symbolErrorRate,31,26)
 
